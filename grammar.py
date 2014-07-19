dadf9b94fbf0507e1dd34b1836768f2c83198a1b
Avram88 <avramscp@gmail.com>
2014-07-20 01:47:13 +0200

    Basic semantic actions

--- grammar.py
+++ grammar.py
@@ -25,7 +25,7 @@ class DSLflow():
     def __init__(self):
         self.parser = ParserPython(workflow, debug=True)
         
-    def create(self, input_workflow):                        
+    def run(self, input_workflow):                        
         try:            
             parse_tree = self.parser.parse(input_workflow)                
             PWDOTExporter().exportFile(parse_tree, "workflow.dot")        
@@ -34,3 +34,49 @@ class DSLflow():
             
         except Exception:
             print "Language error..."
+
+class Workflow(SemanticAction):        
+    def __init__(self, *args, **kwargs):
+        self.tasks = []
+        
+    def first_pass(self, parser, node, children):
+        self.tasks = list(children)
+        return self
+
+class Task(SemanticAction):    
+    def __init__(self, *args, **kwargs):
+        self.name = None
+    
+    def first_pass(self, parser, node, children):
+        self.name = [str(child.value) for child in children if isinstance(child, Name)][0]        
+        return self
+    
+class Name(SemanticAction):    
+    def __init__(self, *args, **kwargs):
+        self.value = None
+    
+    def first_pass(self, parser, node, children):
+        self.value = node
+        return self
+        
+workflow.sem = Workflow()
+task.sem = Task() 
+name.sem = Name()
+
+if __name__ == "__main__":
+    
+    input_workflow = '''    
+               task One() 
+               
+               task Two()
+            '''
+    
+    parser = ParserPython(workflow, debug=True)
+       
+    parse_tree = parser.parse(input_workflow)  
+        
+    workflow_obj = parser.getASG()
+    
+    #insert workflow_obj in database
+    
+    
